1642ff5fe813df4fcb926eaddfd4827e3df46222
Andrew Rogers <abennobashi@gmail.com>
2011-10-18 21:51:18 -0700

    formatting changes

--- src/com/hackathon/photohunt/HelloItemizedOverlay.java
+++ src/com/hackathon/photohunt/HelloItemizedOverlay.java
@@ -9,38 +9,41 @@ import com.google.android.maps.OverlayItem;
 
 public class HelloItemizedOverlay extends ItemizedOverlay<OverlayItem>
 {
- private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
- private Context mContext;
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Context mContext;
 
- public HelloItemizedOverlay(Drawable defaultMarker, Context context)
- {
- super(boundCenterBottom(defaultMarker));
- mContext = context;
- }
+	public HelloItemizedOverlay(Drawable defaultMarker, Context context)
+	{
+		super(boundCenterBottom(defaultMarker));
+		mContext = context;
+	}
 
- public void addOverlay(OverlayItem overlay)
- {
- mOverlays.add(overlay);
- populate();
- }
- @Override
- protected OverlayItem createItem(int i)
- {
- return mOverlays.get(i);
- }
- @Override
- public int size()
- {
- return mOverlays.size();
- }
- @Override
- protected boolean onTap(int index)
- {
- OverlayItem item = mOverlays.get(index);
- AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
- dialog.setTitle(item.getTitle());
- dialog.setMessage(item.getSnippet());
- dialog.show();
- return true;
- }
+	public void addOverlay(OverlayItem overlay)
+	{
+		mOverlays.add(overlay);
+		populate();
+	}
+
+	@Override
+	protected OverlayItem createItem(int i)
+	{
+		return mOverlays.get(i);
+	}
+
+	@Override
+	public int size()
+	{
+		return mOverlays.size();
+	}
+
+	@Override
+	protected boolean onTap(int index)
+	{
+		OverlayItem item = mOverlays.get(index);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
+	}
 }
